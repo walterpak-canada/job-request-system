602f7343a85d78554364d3c0df5b96a6eb9e1607
Walter Pak <walter.pak@gmail.com>
2025-08-13 17:49:44 -0700

    Changes to fix GitHub Actions problem

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/job-request-system/',
+  //base: '/job-request-system/',
   plugins: [
     react(),
     tailwindcss(),
