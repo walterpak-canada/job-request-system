4460d72d213f952e9de10944da966498df2e7224
Walter Pak <walter.pak@gmail.com>
2025-08-13 17:26:42 -0700

    Changes for setting up GitHub page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/job-request-system/',
   plugins: [
     react(),
     tailwindcss(),
